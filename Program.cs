7e0649a1650ac7747fc0f87c4f60b2158d1f5302
Germán <germanbomprezzi@gmail.com>
2022-01-13 10:25:59 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            Console.WriteLine($"Path donde se está ejecutando la app: {path}");
+
+            foreach (var f in new DirectoryInfo(path).GetFiles("*.vb", SearchOption.AllDirectories))
+            {
+                string s = File.ReadAllText(f.FullName);
+                File.WriteAllText(f.FullName, s, System.Text.Encoding.UTF8);
+            }
+
+            foreach (var f in new DirectoryInfo(path).GetFiles("*.cs", SearchOption.AllDirectories))
+            {
+                string s = File.ReadAllText(f.FullName);
+                File.WriteAllText(f.FullName, s, System.Text.Encoding.UTF8);
+            }
+        }
+    }
+}
